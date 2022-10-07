e1edfc4ea7afc24c0667162efad9ad67052d6071
Satvik <satviksengar60@gmail.com>
2022-10-07 15:49:54 +0530

    new code is added

--- Sorting.java
+++ Sorting.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Sorting {
     public static void main(String[] args) {
         int[] arr = {55,33,66,11,88,34};
-        selectionSort(arr);
+        bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
     public static void selectionSort(int[] arr){
@@ -22,11 +22,29 @@ public class Sorting {
     }
 
     public static void bubbleSort(int[] arr){
-
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-i-1 ; j++) {
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
     }
 
     public static void insertionSort(int[] arr){
+        // insert a number to its correct position in sorted array.
+        for(int i = 1; i < arr.length; i++){
+            int curr = arr[i];
+            int j = i-1;
 
+            while(j>=0 && arr[j] > curr ){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = curr;
+        }
     }
 
 }
