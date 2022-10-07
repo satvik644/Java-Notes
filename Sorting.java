a0eaeb527437a871d6bf5004e714819ab5eb2b0c
Satvik <satviksengar60@gmail.com>
2022-10-07 14:48:06 +0530

    sorting file is added

---
+++ Sorting.java
@@ -0,0 +1,19 @@
+package algorithms;
+
+public class Sorting {
+    public static void main(String[] args) {
+
+    }
+    public static void selectionSort(int[] arr){
+        // find the mi
+    }
+
+    public static void bubbleSort(int[] arr){
+
+    }
+
+    public static void insertionSort(int[] arr){
+
+    }
+
+}
